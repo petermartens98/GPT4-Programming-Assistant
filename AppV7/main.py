240d46fc739aaecc06938216dc5ac25296d491ed
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-07-14 10:46:32 -0400

    Update main.py

--- AppV7/main.py
+++ AppV7/main.py
@@ -327,7 +327,7 @@ def display_all_code():
     st.subheader("GitHub Link")
     st.write("https://github.com/petermartens98/GPT4-Programming-Assistant")
     st.subheader("Source Code")
-    files = ["main.py", "df_functions.py", "htmlTemplates.py", "prompts.py", "requirements.txt"]
+    files = ["main.py", "db_functions.py", "htmlTemplates.py", "prompts.py", "requirements.txt"]
     for file in files:
         display_file_code(file)
 
